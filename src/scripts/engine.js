75ca142d464c8ce681f802101e321d75c9fc487d
TPWLU <sppaulisto972@gmail.com>
2024-12-14 18:48:58 -0300

    Update: add function to draw cards

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -16,6 +16,11 @@ const state = {
   button: document.getElementById("next-duel"),
 };
 
+const playerSides = {
+    player1: "player-field-card",
+    computer: "computer-field-card",
+}
+
 const pathImages = ".src/assets/icons/";
 
 const cardData = [
@@ -45,8 +50,18 @@ const cardData = [
   },
 ];
 
-function init() {
+async function drawCards(cardNumber, fieldSide) {
+    for (let i = 0; i < cardNumber; i++) {
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
 
+        document.getElementById(fieldSide).appendChild(cardImage);
+    }
+}
+
+function init() {
+    drawCards(5, playerSides.player1);
+    drawCards(5, playerSides.computer);
 }
 
 init();
